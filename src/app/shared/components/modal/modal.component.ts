83bd7e1c09c1042d06ba6cb8cf60707f8e7ac009
Andrzej <glogo85@gmail.com>
2024-02-17 18:52:26 +0100

    implemented: ngAfterContentInit and ngAfterContentChecked for modal/todo components
    implemented: ngAfterContentInit and ngAfterContentChecked for modal/todo components

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,13 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements AfterContentInit, AfterContentChecked {
+
   @Input() title!: string;
   @Output() close = new EventEmitter<void>();
+  @ContentChild('modalDiv') modalDiv!: ElementRef;
+  @ContentChild('check ') checkbox!: ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit test: ' + this.modalDiv);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log(this.checkbox.nativeElement.checked);
+  }
 
   onClose(){
     this.close.emit();
