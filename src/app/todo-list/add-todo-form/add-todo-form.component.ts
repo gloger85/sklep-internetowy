f1d51cc1fe3f7268883a28cdf94cffdfa4122b6f
Andrzej <glogo85@gmail.com>
2024-05-15 22:46:04 +0200

    add directive to validate first sign as letter
    add directive to validate first sign as letter

--- src/app/todo-list/add-todo-form/add-todo-form.component.ts
+++ src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -1,22 +1,19 @@
-import { AfterContentInit, Component, EventEmitter, Output, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-add-todo-form',
   templateUrl: './add-todo-form.component.html',
   styleUrls: ['./add-todo-form.component.css']
 })
-export class AddTodoFormComponent implements AfterContentInit{
+export class AddTodoFormComponent {
 
 @Output() addTodo = new EventEmitter<string>();
-@ViewChild('form') todoForm!: NgForm;
 todoName ='';
+//person = {name: 'test', surname: 'testowy', role: 'father'}
 
-addNewTodo(form:NgForm) {
-
+addNewTodo() {
+//console.log(this.person);
   this.addTodo.emit(this.todoName);
 }
-ngAfterContentInit(): void {
-  console.log(this.todoForm);
-}
+
 }
